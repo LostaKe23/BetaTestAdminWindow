f67b260e20cc59e25e4dd2cf35b7ee49b1f0b2ba
LostaKe23 <162482713+LostaKe23@users.noreply.github.com>
2025-01-09 14:53:41 +0200

    Add files via upload

---
+++ ProjectAdminWindow v1/Model/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAdminWindow_v1.Client
+{
+    public class Client
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public int BirthYear { get; set; }
+        public string EGRN { get; set; }
+        public string Status { get; set; } // Варианты статусов "Ожидает проверки", "Проверка пройдена", "Проверка не пройдена"
+    }
+}
---
+++ ProjectAdminWindow v1/ViewModel/ClientViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Net.Http.Json;
+
+namespace ProjectAdminWindow_v1.ViewModel
+{
+    public class ClientsViewModel : INotifyPropertyChanged
+    {
+    private ObservableCollection<Client> _clients;
+        private readonly HttpClient _client;
+
+        public ObservableCollection<Client> Clients
+        {
+            get => _clients;
+            set
+            {
+                _clients = value;
+                OnPropertyChanged(nameof(Clients));
+            }
+        }
+
+        public ClientsViewModel()
+        {
+            _client = new HttpClient();
+            LoadClients();
+        }
+
+        private async void LoadClients()
+        {
+            // Загрузка данных из базы данных
+            // Примерный URL API для получения данных
+            var response = await _client.GetAsync("http://example.com/api/clients");
+            if (response.IsSuccessStatusCode)
+            {
+                var clientsData = await response.Content.ReadAsAsync<List<Client>>();
+                Clients = new ObservableCollection<Client>(clientsData);
+            }
+        }
+
+        public async void ConfirmClient(Client client)
+        {
+            client.Status = "Проверка пройдена";
+            await UpdateClientStatus(client);
+        }
+
+        public async void RejectClient(Client client)
+        {
+            client.Status = "Проверка не пройдена";
+            await UpdateClientStatus(client);
+        }
+
+        private async Task UpdateClientStatus(Client client)
+        {
+            // Отправка обновленного состояния клиента на сервер
+            await _client.PutAsJsonAsync($"http://example.com/api/clients/{client.EGRN}", client);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
